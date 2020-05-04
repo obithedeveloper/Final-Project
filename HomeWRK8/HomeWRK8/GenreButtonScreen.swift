13fe7b3126c7885b4b440f85b9414c3465a28c49
Obinna <inlandobinna@gmail.com>
2020-05-04 10:52:00 -0500

    ready to grade

---
+++ HomeWRK8/HomeWRK8/GenreButtonScreen.swift
@@ -0,0 +1,54 @@
+//
+//  GenreButtonScreen.swift
+//  HomeWRK8
+//
+//  Created by Obinna Ezekwesili on 5/2/20.
+//  Copyright © 2020 Obinna Ezekwesili. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UITabBarController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                self.playGenre(genre: sender.currentTitle!)
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String){
+        musicPlayer.stop()
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+        
+        
+    }
+    
+}
